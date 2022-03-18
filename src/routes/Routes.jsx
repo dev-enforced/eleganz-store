4903a4b7bca62c44add0d88bfa2e1fc0b219fb1c
Roshan J <devenforced@gmail.com>
2022-03-18 20:16:58 +0530

    formatted routes collection file

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import {Mockbee,MockMan} from "../pages"
-const RoutesList=()=>{
-    return(
+import { Mockbee, MockMan } from "../pages"
+const RoutesList = () => {
+    return (
         <Routes>
             <Route path="/mockbee" element={<Mockbee />} />
-        <Route path="/mockman" element={<MockMan />} />
+            <Route path="/mockman" element={<MockMan />} />
         </Routes>
     )
 }
-export {RoutesList}
+export { RoutesList }
