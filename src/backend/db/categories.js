94700c5a999e19e2334dd3519fbf0bd9da5f1425
Roshan J <devenforced@gmail.com>
2022-03-23 04:17:39 +0530

    refactor:image links for categories fixed

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -10,29 +10,29 @@ export const categories = [
     _id: uuid(),
     categoryName: "Tshirts",
     tagLine: "WEAR YOUR BEST MOMENTS",
-    imgUrl: "https://cdn.shopify.com/s/files/1/0220/4008/4552/products/staple_6pack_1024x1024.jpg?v=1640718788"
+    imgUrl: "https://i.postimg.cc/NM3jBk6m/staple-6pack-1024x1024.jpg"
   },
   {
     _id: uuid(),
     categoryName: "Casual Pants",
     tagLine: "Simple Yet Elegant",
-    imgUrl: "https://ae01.alicdn.com/kf/H0218744e90d1406288714f11429ea24ex/2020-New-Men-s-Cotton-Linen-Pants-Male-Summer-Breathable-Solid-Color-Linen-Trousers-Fitness-Streetwear.jpg_Q90.jpg_.webp"
+    imgUrl: "https://i.postimg.cc/QMMtbtN9/2020-New-Men-s-Cotton-Linen-Pants-Male-Summer-Breathable-Solid-Color-Linen-Trousers-Fitness-Streetwe.webp"
   },{
     _id: uuid(),
     categoryName: "Shirts",
     tagLine:"LET YOUR OUTFIT SHOW YOUR SUCCESS",
-    imgUrl:"https://www.myraymond.com/media/product/PWSU01983-R6/PWSU01983-R6%20(2).jpg"
+    imgUrl:"https://i.postimg.cc/QM7F78QZ/PWSU01983-R6-2.jpg"
   },
   {
     _id:uuid(),
     categoryName:"Dresses",
     tagLine:"Discover the inner Diva",
-    imgUrl:"https://www.myraymond.com/media/product/PWEX00881-K8/PWEX00881-K8%20(1).jpg"
+    imgUrl:"https://i.postimg.cc/QM7F78QZ/PWSU01983-R6-2.jpg"
   },
   {
     _id:uuid(),
     categoryName:"Jackets and Coats",
     tagLine:"Where Fashion Meets Class",
-    imgUrl:"https://www.myraymond.com/media/product/PMJS02006-O7/PMJS02006-O7%20(1).jpg"
+    imgUrl:"https://i.postimg.cc/QM7F78QZ/PWSU01983-R6-2.jpg"
   }
 ];
