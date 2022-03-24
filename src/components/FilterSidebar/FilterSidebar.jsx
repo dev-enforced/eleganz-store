31c59000c0cd4e28f8679d6916f6c6999819fe65
Roshan J <devenforced@gmail.com>
2022-03-24 13:37:05 +0530

    splitted the product list page into the respective component and added the routes to it

---
+++ src/components/FilterSidebar/FilterSidebar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./FilterSidebar.css";
+
+const FilterSidebar=()=>{
+    return (
+        <>
+        
+        </>
+    )
+}
+export {FilterSidebar};
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
 export { Footer } from "./Footer/Footer";
 export { Navigation } from "./Navigation/Navigation";
 export {Hero} from "./Hero/Hero";
-export {Categories} from "./Categories/Categories"
+export {Categories} from "./Categories/Categories";
+export {ProductCard} from "./ProductCard/ProductCard";
+export {ProductList} from "./ProductList/ProductList";
+export {FilterSidebar} from "./FilterSidebar/FilterSidebar";
