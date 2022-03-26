d2fbef8b269537550b2195af6c48acb1f5ba075f
Roshan J <devenforced@gmail.com>
2022-03-26 18:00:34 +0530

    adding the changes

--- src/components/FilterSidebar/FilterSidebar.jsx
+++ src/components/FilterSidebar/FilterSidebar.jsx
@@ -1,8 +1,11 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
+import { useProducts } from "context/ProductContext";
 import "./FilterSidebar.css";
 
 const FilterSidebar = () => {
-    const [mobileFilterView,setMobileFilterView]=useState(false);
+    const [mobileFilterView, setMobileFilterView] = useState(false);
+    const { filterState, filterDispatch } = useProducts();
+    const { sortPriceOrder, gender,categories } = filterState
     return (
         <aside className="filter-sidebar-container p-10">
             <div className="filter-sidebar-content gentle-flex-gap flex-column">
@@ -10,55 +13,55 @@ const FilterSidebar = () => {
                     <h3 className="txt-sm">
                         FILTERS
                     </h3>
-                    <h3 className="txt-sm mobile-view-filter-button" onClick={()=>setMobileFilterView(currentValue=>!currentValue)}>
+                    <h3 className="txt-sm mobile-view-filter-button" onClick={() => setMobileFilterView(currentValue => !currentValue)}>
                         SHOW
                     </h3>
-                    <h3 className="txt-xs clear-filter">
+                    <h3 className="txt-xs clear-filter" onClick={() => { filterDispatch({ type: "CLEAR-ALL" }) }}>
                         CLEAR ALL
                     </h3>
                 </div>
-                <div className={`side-nav ${mobileFilterView?"active":""} flex-column pb-3`}>
+                <div className={`side-nav ${mobileFilterView ? "active" : ""} flex-column pb-3`}>
                     <h3 className="txt-sm mb-3">SORT BY</h3>
                     <div className="gentle-flex-gap flex-align-center mx-6">
-                        <input type="radio" id="low-to-high" name="sort-price" />
+                        <input type="radio" id="low-to-high" name="sort-price" checked={sortPriceOrder === "Low To High"} onChange={() => { filterDispatch({ type: "LOW-TO-HIGH" }) }} />
                         <label htmlFor="low-to-high">LOW TO HIGH</label>
                     </div>
                     <div className="gentle-flex-gap flex-align-center mx-6">
-                        <input type="radio" id="high-to-low" name="sort-price" />
+                        <input type="radio" id="high-to-low" name="sort-price" checked={sortPriceOrder === "High To Low"} onChange={() => { filterDispatch({ type: "HIGH-TO-LOW" }) }} />
                         <label htmlFor="high-to-low">HIGH TO LOW</label>
                     </div>
                 </div>
                 <div className={`side-nav ${mobileFilterView ? "active" : ""} flex-column py-3`}>
                     <h3 className="txt-sm mb-3">CATEGORIES</h3>
                     <div className="gentle-flex-gap flex-align-center mx-6">
-                        <input type="checkbox" id="Tshirts" name="sort-category" />
+                        <input type="checkbox" id="Tshirts" name="sort-category" checked={categories.includes("Tshirts")} onChange={(e) => e.target.checked ? filterDispatch({ type: "TSHIRTS" }) : filterDispatch({ type: "REMOVE-TSHIRTS" })} />
                         <label htmlFor="Tshirts">Tshirts</label>
                     </div>
                     <div className="gentle-flex-gap flex-align-center mx-6">
-                        <input type="checkbox" id="Shirts" name="sort-category" />
+                        <input type="checkbox" id="Shirts" name="sort-category" checked={categories.includes("Shirts")} onChange={(e) => e.target.checked ? filterDispatch({ type: "SHIRTS" }) : filterDispatch({ type: "REMOVE-SHIRTS" })} />
                         <label htmlFor="Shirts">Shirts</label>
                     </div>
                     <div className="gentle-flex-gap flex-align-center mx-6">
-                        <input type="checkbox" id="Jackets" name="sort-category" />
+                        <input type="checkbox" id="Jackets" name="sort-category" checked={categories.includes("Jackets and Coats")} onChange={(e) => e.target.checked ? filterDispatch({ type: "JACKETS" }) : filterDispatch({ type: "REMOVE-JACKETS" })} />
                         <label htmlFor="Jackets">Jackets and Coats</label>
                     </div>
                     <div className="gentle-flex-gap flex-align-center mx-6">
-                        <input type="checkbox" id="Dresses" name="sort-category" />
+                        <input type="checkbox" id="Dresses" name="sort-category" checked={categories.includes("Dresses")} onChange={(e) => e.target.checked ? filterDispatch({ type: "DRESSES" }) : filterDispatch({ type: "REMOVE-DRESSES" })} />
                         <label htmlFor="Dresses">Dresses</label>
                     </div>
                     <div className="gentle-flex-gap flex-align-center mx-6">
-                        <input type="checkbox" id="Pants" name="sort-category" />
+                        <input type="checkbox" id="Pants" name="sort-category" checked={categories.includes("Casual Pants")} onChange={(e) => e.target.checked ? filterDispatch({ type: "PANTS" }) : filterDispatch({ type: "REMOVE-PANTS" })} />
                         <label htmlFor="Pants">Casual Pants</label>
                     </div>
                 </div>
                 <div className={`side-nav ${mobileFilterView ? "active" : ""} flex-column pb-3`}>
                     <h3 className="txt-sm mb-3">WORN BY</h3>
                     <div className="gentle-flex-gap flex-align-center mx-6">
-                        <input type="radio" id="men-garments" name="worn-by" />
+                        <input type="radio" id="men-garments" name="worn-by" checked={gender === "Men"} onChange={() => { filterDispatch({ type: "MEN" }) }} />
                         <label htmlFor="men-garments">Men</label>
                     </div>
                     <div className="gentle-flex-gap flex-align-center mx-6">
-                        <input type="radio" id="women-garments" name="worn-by" />
+                        <input type="radio" id="women-garments" name="worn-by" checked={gender === "Women"} onChange={() => { filterDispatch({ type: "WOMEN" }) }} />
                         <label htmlFor="women-garments">Women</label>
                     </div>
                 </div>
--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { createContext, useContext, useEffect, useState } from "react";
+import React, { createContext, useContext, useEffect, useState, useReducer } from "react";
 const ProductContext = createContext();
 const useProducts = () => useContext(ProductContext);
 
@@ -13,12 +13,88 @@ const ProductProvider = ({ children }) => {
             console.log("Error")
         }
     }
+    const initialFilterState = {
+        sortPriceOrder: "",
+        gender: "",
+        categories: []
+    }
+
+    const filterReducer = (givenState, action) => {
+        switch (action.type) {
+            case "LOW-TO-HIGH":
+                return { ...givenState, sortPriceOrder: "Low To High" }
+            case "HIGH-TO-LOW":
+                return { ...givenState, sortPriceOrder: "High To Low" }
+            case "MEN":
+                return { ...givenState, gender: "Men" }
+            case "WOMEN":
+                return { ...givenState, gender: "Women" }
+            case "TSHIRTS":
+                return { ...givenState, categories: !givenState.categories.includes("Tshirts") ? [...givenState.categories, "Tshirts"] : [...givenState.categories] }
+            case "REMOVE-TSHIRTS":
+                return { ...givenState, categories: givenState.categories.filter((everyCategory) => everyCategory !== "Tshirts") }
+            case "SHIRTS":
+                return { ...givenState, categories: !givenState.categories.includes("Shirts") ? [...givenState.categories, "Shirts"] : [...givenState.categories] }
+            case "REMOVE-SHIRTS":
+                return { ...givenState, categories: givenState.categories.filter((everyCategory) => everyCategory !== "Shirts") }
+            case "DRESSES":
+                return { ...givenState, categories: !givenState.categories.includes("Dresses") ? [...givenState.categories, "Dresses"] : [...givenState.categories] }
+            case "REMOVE-DRESSES":
+                return { ...givenState, categories: givenState.categories.filter((everyCategory) => everyCategory !== "Dresses") }
+            case "JACKETS":
+                return { ...givenState, categories: !givenState.categories.includes("Jackets and Coats") ? [...givenState.categories, "Jackets and Coats"] : [...givenState.categories] }
+            case "REMOVE-JACKETS":
+                return { ...givenState, categories: givenState.categories.filter((everyCategory) => everyCategory !== "Jackets and Coats") }
+            case "PANTS":
+                return { ...givenState, categories: !givenState.categories.includes("Casual Pants") ? [...givenState.categories, "Casual Pants"] : [...givenState.categories] }
+            case "REMOVE-PANTS":
+                return { ...givenState, categories: givenState.categories.filter((everyCategory) => everyCategory !== "Casual Pants") }
+            case "CLEAR-ALL":
+                return { ...initialFilterState }
+            default:
+                return givenState
+        }
+    }
+    const [filterState, filterDispatch] = useReducer(filterReducer, initialFilterState);
+
+    const applySortingChoice = (givenState, providedProducts) => {
+        if (givenState.sortPriceOrder === "Low To High") {
+            return [...providedProducts].sort((a, b) => Number(a.discountedPrice) - Number(b.discountedPrice))
+        } else if (givenState.sortPriceOrder === "High To Low") {
+            return [...providedProducts].sort((a, b) => Number(b.discountedPrice) - Number(a.discountedPrice))
+        } else {
+            return [...providedProducts]
+        }
+    }
+
+    const applyCategoryChoice = (givenState, providedProducts) => {
+        const { categories } = givenState;
+        return categories.length > 0 ? [...providedProducts].filter((everyProduct) => categories.includes(everyProduct.categoryName)): [...providedProducts]
+    }
+
+    const applyGenderChoice = (givenState, providedProducts) => {
+        if (givenState.gender === "Men") {
+            return [...providedProducts].filter((everyItem) => everyItem.wornBy === "Men")
+        } else if (givenState.gender === "Women") {
+            return [...providedProducts].filter((everyItem) => everyItem.wornBy === "Women")
+        } else {
+            return [...providedProducts]
+        }
+    }
+
+    const cumulativeFilters = (...participatingFilters) => {
+        return (givenState, providedProducts) => {
+            return participatingFilters.reduce((previousFilterResult, currentFilter) => currentFilter(givenState, previousFilterResult), providedProducts)
+        }
+    }
+
+
     useEffect(() => {
         loadProducts()
     }, [])
-    const newProducts = [...productsDb];
+    const newProducts = cumulativeFilters(applySortingChoice, applyGenderChoice, applyCategoryChoice)(filterState, [...productsDb]);
     return (
-        <ProductContext.Provider value={{ newProducts }}>
+        <ProductContext.Provider value={{ newProducts, filterState, filterDispatch }}>
             {children}
         </ProductContext.Provider>
     )
