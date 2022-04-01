9c1f79745074bf352f0c73cb056e0b495ea65dde
Roshan J <devenforced@gmail.com>
2022-04-01 13:59:38 +0530

    refactor:formatted code

--- src/services/Cart/updateItemQuantity.js
+++ src/services/Cart/updateItemQuantity.js
@@ -1,17 +1,18 @@
 import axios from "axios";
 const quantityUpdate = async (authStateGiven, authDispatchFunction, productDetails, typeGiven) => {
     try {
-        const { data: {
-            cart: cartUpdated
-        } } = await axios.post(`/api/user/cart/${productDetails._id}`, {
-            action: {
-                type: typeGiven
-            }
-        }, {
-            headers: {
-                authorization: authStateGiven.authenticationToken
-            }
-        })
+        const {
+            data: {
+                cart: cartUpdated
+            } } = await axios.post(`/api/user/cart/${productDetails._id}`, {
+                action: {
+                    type: typeGiven
+                }
+            }, {
+                headers: {
+                    authorization: authStateGiven.authenticationToken
+                }
+            })
         authDispatchFunction({ type: "ADD-TO-CART", payload: cartUpdated })
     } catch (error) {
         console.error("Error while doing quantity update through API")
