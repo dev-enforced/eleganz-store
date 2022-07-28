b0296b78c0cb731786c0b0788be39f9894445eab
Roshan J <devenforced@gmail.com>
2022-07-29 03:53:27 +0530

    [chore]: new backend controller addition for clear cart

--- src/backend/controllers/CartController.js
+++ src/backend/controllers/CartController.js
@@ -146,3 +146,30 @@ export const updateCartItemHandler = function (schema, request) {
     );
   }
 };
+//  clear cart controller
+
+export const clearCartHandler = function (schema, request) {
+  const userId = requiresAuth.call(this, request);
+  try {
+    if (!userId) {
+      return new Response(
+        404,
+        {},
+        {
+          errors: ["The email you entered is not Registered. Not Found error"],
+        }
+      );
+    }
+    //const userCart = schema.users.findBy({ _id: userId }).cart;
+    this.db.users.update({ cart: [] });
+    return new Response(200, {}, { cart: [] });
+  } catch (error) {
+    return new Response(
+      500,
+      {},
+      {
+        error,
+      }
+    );
+  }
+};
