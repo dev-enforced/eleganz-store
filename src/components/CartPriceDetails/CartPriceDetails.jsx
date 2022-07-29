238f0f98cca22eb8c7a037382a8ac11608c06450
Roshan J <devenforced@gmail.com>
2022-07-29 18:51:28 +0530

    feat: payment gateway made functional with amount and edge cases of failure dismiss and success through toasts

--- src/components/CartPriceDetails/CartPriceDetails.jsx
+++ src/components/CartPriceDetails/CartPriceDetails.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-hot-toast";
 import { useAuthentication } from "context";
 import "./CartPriceDetails.css";
+import { routes } from "constants";
 
 const CartPriceDetails = () => {
+  const { HOME_ROUTE } = routes;
   const {
     authState: { cart: cartProvided },
+    clearWholeCartAction,
   } = useAuthentication();
+  const navigateTo = useNavigate();
   const priceSum = cartProvided.reduce(
     (totalPrice, everyCartItem) => {
       return {
@@ -20,6 +26,48 @@ const CartPriceDetails = () => {
     },
     { original: 0, discount: 0 }
   );
+  const successfulTransaction = (paymentGatewayResponse) => {
+    toast.success("Checked out successfully. Products will be shipped soon");
+    return paymentGatewayResponse;
+  };
+  const failedTransaction = (paymentGatewayResponse) => {
+    toast.error("Transaction failed");
+    return paymentGatewayResponse;
+  };
+  const displayPaymentGateway = (transactionAmount) => {
+    const options = {
+      // eslint-disable-next-line no-undef
+      key: process.env.REACT_APP_PAYMENT_GATEWAY_SECRET,
+      amount: transactionAmount * 100,
+      currency: "INR",
+      name: "Eleganz Store",
+      description: "Thank you for shopping with us",
+      image: "assets/eleganz_logo.png",
+      handler: (response) => {
+        successfulTransaction(response);
+        clearWholeCartAction();
+        navigateTo(HOME_ROUTE);
+      },
+      modal: {
+        onDismiss: () => {
+          toast("Transaction cancelled");
+        },
+      },
+      prefill: {
+        name: "Guest User",
+        email: "onlineguest@gmail.com",
+        contact: 9127108910,
+      },
+      theme: {
+        color: "#7e22ce",
+      },
+    };
+    const razorpayEmbed = new window.Razorpay(options);
+    razorpayEmbed.open();
+    razorpayEmbed.on("payment.failed", (response) => {
+      failedTransaction(response);
+    });
+  };
   return cartProvided.length === 0 ? (
     ""
   ) : (
@@ -39,6 +87,14 @@ const CartPriceDetails = () => {
           <p>{priceSum.discount}</p>
         </div>
       </div>
+      <button
+        className="my-2 cart-btn-checkout"
+        onClick={() => {
+          displayPaymentGateway(priceSum.discount);
+        }}
+      >
+        CHECKOUT
+      </button>
     </div>
   );
 };
